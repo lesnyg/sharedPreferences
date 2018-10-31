3a6a654904f7125e6fe3a48c984724b66981546b
이선영 <lsy79@hanmail.net>
2018-10-31 15:03:50 +0900

    commit2

--- app/src/main/java/com/lesnyg/edu/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/lesnyg/edu/sharedpreferences/MainActivity.java
@@ -1,13 +1,37 @@
 package com.lesnyg.edu.sharedpreferences;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttoninput = findViewById(R.id.buttoninput);
+        buttoninput.setOnClickListener(this);
+        Button buttonoutput = findViewById(R.id.buttonoutput);
+        buttonoutput.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttoninput:
+                Intent intent = new Intent(this, inputActivity.class);
+                startActivityForResult(intent,1000);
+                break;
+            case R.id.buttonoutput:
+                Intent intent1 = new Intent(this, inputActivity.class);
+                startActivityForResult(intent1,1000);
+                break;
+        }
     }
 }
